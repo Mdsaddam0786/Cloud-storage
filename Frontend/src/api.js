de25952e90807e244511eb3aae70579fc111f38a
Md Saddam <91770727+Mdsaddam0786@users.noreply.github.com>
2025-08-12 01:53:30 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // ✅ match your backend
+  baseURL: 'https://cloud-storage-production-2ef6.up.railway.app/', // ✅ match your backend
 });
 
 api.interceptors.request.use((config) => {
